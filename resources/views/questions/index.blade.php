6b50ae43f3a1df90f65b966ef6c5f73b50e7ec6e
Asa hari wibowo <asahari.wibowo@gmail.com>
2019-12-30 14:01:43 +0700

    implementation validitation dan store the question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -7,39 +7,46 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
-                        <h2>Ask Question</h2>
+                        <h2>All Questions</h2>
                         <div class="ml-auto">
-                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to All Question</a>
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
                         </div>
+                    </div>
+                    
                 </div>
-            </div>
 
-                <div class="card-body">
-                    <form action="{{ route('questions.store') }}" method="post">
-                        @csrf
-                        <div class="form-group">
-                            <label for="question-title">question Title</label>
-                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+                <div class="card-body">     
+                    @include ('layouts._messages')
 
-                            @if ($errors->has('title'))
-                                <div class="invalid-feedback">
-                                    <strong> {{ $errors->first('title') }} </strong>
-                                </div>
-                            @endif
+                    @foreach ($questions as $question)
+                        <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
+                                </div>                            
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
+                                </div>                            
+                                <div class="view">
+                                    {{ $question->views . " " . str_plural('view', $question->views) }}
+                                </div>                            
+                            </div>
+                            <div class="media-body">
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <p class="lead">
+                                    Asked by 
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a> 
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
+                                {{ str_limit($question->body, 250) }}
+                            </div>                        
                         </div>
-                        <div class="form-group">
-                            <label for="question-body">Explain your question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
-                        </div>
+                        <hr>
+                    @endforeach
 
-                            @if ($errors->has('body'))
-                                <div class="invalid-feedback">
-                                    <strong> {{ $errors->first('title') }} </strong>
-                                </div>
-                            @endif
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask this question</button>
-                    </form>
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
+                    </div>
                 </div>
             </div>
         </div>
