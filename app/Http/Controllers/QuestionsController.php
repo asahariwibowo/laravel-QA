e84544014aa4a9f79d7da57b70316de5bd7fc903
Asa hari wibowo <asahari.wibowo@gmail.com>
2020-01-02 12:55:23 +0700

    Design answers table schema and rename answers coloumn to answers_count in the table questions

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -10,7 +10,7 @@ use App\Http\Requests\AskQuestionRequest;
 class QuestionsController extends Controller
 {
     public function __construct() {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth', ['except' => ['index', 'show']]); 
     }
     /**
      * Display a listing of the resource.
